2be5a5ff682de8a0e5b442485249701d649eda83
David Lopez Miguel <david.lopez.miguel@bbva.com>
2017-04-02 10:54:52 +0200

    large payload

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"math/rand"
 	"runtime"
 
 	"google.golang.org/grpc"
@@ -14,6 +15,8 @@ const (
 	port = ":50051"
 
 	events = 1000 * 1000 * 1000
+
+	payload_size = 1024
 )
 
 // server is used to implement StreamingServiceClient
@@ -21,12 +24,14 @@ type server struct{}
 
 func (s *server) StreamIt(in *Empty, downstream StreamingService_StreamItServer) error {
 
+	payload := RandStringBytes(payload_size)
+
 	for i := 0; i < events; i++ {
 		ms := runtime.MemStats{}
 		runtime.ReadMemStats(&ms)
 		fmt.Printf("\rServing. Alloc %10d, Sys %10d.", ms.Alloc, ms.Sys)
 
-		err := downstream.Send(&Event{12345})
+		err := downstream.Send(&Event{payload})
 		if err != nil {
 			fmt.Println()
 			log.Fatalf("error sending event: %v", err)
@@ -48,4 +53,14 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
